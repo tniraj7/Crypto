6898e1aabbfc85b70933b62b2efeab8dac12fce7
Niraj Tiwari <tniraj7@gmail.com>
2019-11-17 22:05:10 +0900

    Add cryptocurrenies arrray of type Coin

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -1,6 +1,19 @@
 import SwiftUI
 
+struct Coin: Identifiable{
+    let id, name, price, icon: String
+}
+
 struct ContentView: View {
+    
+    var rates : [Coin] = [
+        Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin"),
+        Coin(id: "LTC", name: "Litecoin", price: "78.70", icon: "litecoin"),
+        Coin(id: "XRP", name: "Ripple", price: "0.30", icon: "ripple"),
+        Coin(id: "TRX", name: "Tron", price: "0.02", icon: "tron"),
+        Coin(id: "ETH", name: "Ethereum", price: "78.70", icon: "ethereum")
+    ]
+    
     var body: some View {
         
         NavigationView {
@@ -11,8 +24,14 @@ struct ContentView: View {
                     .font(.largeTitle)
                 
                 List {
-                    Text("Item 1")
-                    Text("Item 2")
+                    Section(header: Text("Current Prices")) {
+                        ForEach(rates) { coin in
+                            HStack {
+                                Text("\(coin.name) (\(coin.id))")
+                                Text(coin.price)
+                            }
+                        }
+                    }
                 }.listStyle(GroupedListStyle())
             }
             
