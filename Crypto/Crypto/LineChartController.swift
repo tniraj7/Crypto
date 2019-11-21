03feefdc240713f2ee4a71ca66fe12a5be6ef94c
Niraj Tiwari <tniraj7@gmail.com>
2019-11-21 22:45:23 +0900

    implement LineChartViewController

--- Crypto/Crypto/LineChartController.swift
+++ Crypto/Crypto/LineChartController.swift
@@ -1,6 +1,16 @@
-import Foundation
+import SwiftUI
 import UIKit
 
 struct LineChartController: UIViewRepresentable {
     
+    func updateUIView(_ uiView: LineChart, context: UIViewRepresentableContext<LineChartController>) {
+        
+    }
+    
+    func makeUIView(context: Context) -> LineChart {
+        let lineChart = LineChart()
+        lineChart.addLine([3,4,2,1,6,5])
+        return lineChart
+        
+    }
 }
