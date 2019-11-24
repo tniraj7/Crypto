976223df35b116a54ea11b28d3496e7e463cb4d6
Niraj Tiwari <tniraj7@gmail.com>
2019-11-24 19:40:49 +0900

    use dummy lineCoordinates for wallet coins and current prices

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -7,7 +7,6 @@ struct GraphCoin: View {
     let lineCoordinates: [CGFloat]
     
     var body: some  View {
-        LineChartController(lineCoordinates: [5000,8000,7000,11000,5000,6000,10000], inline: false)
         LineChartController(lineCoordinates: lineCoordinates, inline: false)
             .padding(.leading, 30)
         .navigationBarTitle(Text(title))
@@ -17,6 +16,7 @@ struct GraphCoin: View {
 
 struct Coin: Identifiable {
     let id, name, price, icon: String
+    let lineCoordinates: [CGFloat]
 }
 
 struct BadgeSymbol: View {
@@ -60,17 +60,18 @@ struct BadgeSymbol: View {
 struct ContentView: View {
     
     var rates : [Coin] = [
-        Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin"),
-        Coin(id: "LTC", name: "Litecoin", price: "78.70", icon: "litecoin"),
-        Coin(id: "XRP", name: "Ripple", price: "0.30", icon: "ripple"),
-        Coin(id: "TRX", name: "Tron", price: "0.02", icon: "tron"),
-        Coin(id: "ETH", name: "Ethereum", price: "78.70", icon: "ethereum")
+        Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin", lineCoordinates: [2000,8000,4000,9000,5000,6000,3000]),
+        Coin(id: "LTC", name: "Litecoin", price: "78.70", icon: "litecoin", lineCoordinates: [20,10,40,70,50,60,30]),
+        Coin(id: "XRP", name: "Ripple", price: "0.30", icon: "ripple", lineCoordinates: [2.0,1.0,4.0,7.0,5.0,6.0,3.0]),
+        Coin(id: "TRX", name: "Tron", price: "0.02", icon: "tron", lineCoordinates: [0.12,0.41,0.74,0.37,0.15,0.96,0.23]),
+        Coin(id: "ETH", name: "Ethereum", price: "78.70", icon: "ethereum", lineCoordinates: [20.00,10.00,40.00,70.00,50.00,60.00,30.0])
     ]
     
     var myWallet: [Coin] = [
-    Coin(id: "BTC", name: "Bitcoin", price: "1000.0", icon: "bitcoin"),
-    Coin(id: "LTC", name: "Litecoin", price: "2000.0", icon: "litecoin"),
-    Coin(id: "TRX", name: "Tron", price: "133.7", icon: "tron"),
+    Coin(id: "BTC", name: "Bitcoin", price: "1000.0", icon: "bitcoin", lineCoordinates: [2000,1000,4000,7000,5000,6000,3000]),
+    Coin(id: "LTC", name: "Litecoin", price: "2000.0", icon: "litecoin", lineCoordinates: [20,10,40,70,50,60,30]),
+    Coin(id: "TRX", name: "Tron", price: "133.7", icon: "tron",
+        lineCoordinates: [0.02,0.01,0.04,0.07,0.05,0.06,0.03]),
     ]
     
     @State var is360 = false
@@ -119,7 +120,7 @@ struct ContentView: View {
                     Section(header: Text("Current Prices")) {
                         ForEach(rates) { coin in
                             
-                            NavigationLink(destination: GraphCoin(title: coin.name)) {
+                            NavigationLink(destination: GraphCoin(title: coin.name, lineCoordinates: coin.lineCoordinates)) {
 
                                 HStack {
                                     Image(coin.icon)
