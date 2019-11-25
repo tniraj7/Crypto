4ef65924ebd570474a1d8262f9ad27f1075da526
Niraj Tiwari <tniraj7@gmail.com>
2019-11-25 21:16:35 +0900

    Make a login form

---
+++ Crypto/Crypto/LoginView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
