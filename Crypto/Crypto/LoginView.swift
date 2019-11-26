ee691e8d222b1ff1efd765379129165bfe66903d
Niraj Tiwari <tniraj7@gmail.com>
2019-11-26 23:23:17 +0900

    create a class for HTTP networking

--- Crypto/Crypto/LoginView.swift
+++ Crypto/Crypto/LoginView.swift
@@ -1,4 +1,34 @@
 import SwiftUI
+import Combine
+
+
+class HttpAuth: ObservableObject {
+    
+    var didChange = PassthroughSubject<HttpAuth, Never>()
+    
+    var authenticated = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    func checkDetails(username: String, password : String) {
+        guard let url = URL(string: "") else { return }
+        
+        let body = ["username": username, "password": password]
+        
+        let finalBody = try! JSONSerialization.data(withJSONObject: body)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalBody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            
+        }
+}
 
 struct LoginView: View {
     
