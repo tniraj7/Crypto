e0b6a1cfbfb98554e20106426e2c4ae450f30b4b
Niraj Tiwari <tniraj7@gmail.com>
2019-11-26 23:24:13 +0900

    add strcuture for server message

--- Crypto/Crypto/LoginView.swift
+++ Crypto/Crypto/LoginView.swift
@@ -2,6 +2,10 @@ import SwiftUI
 import Combine
 
 
+struct ServerMessage: Decodable {
+    let status, message: String
+}
+
 class HttpAuth: ObservableObject {
     
     var didChange = PassthroughSubject<HttpAuth, Never>()
